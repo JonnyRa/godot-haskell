68aeae8d83dbe749fa50d788b664fc1990b0a816
Jonny Ramsden <JonnyRa@users.noreply.github.com>
2026-08-05 13:01:23 +0100

    start writing of the stuff down in executable form rather than just fiddling about in a terminal

---
+++ godot.bash
@@ -0,0 +1,19 @@
+source ~/.profile
+
+unset -f sourceProfile
+
+sourceProfile(){
+  source "${BASH_SOURCE%/*}"
+}
+
+installHaskellGenerator(){
+  #--copy-bins is effectively `stack install`
+  stackBuildWatch godot-haskell:exe:godot-haskell-project-generator --copy-bins
+}
+
+#run this after changing version
+rebuildGodotTypes () {
+  cd classgen
+  stack build --colour always --file-watch --watch-all --exec  "stack exec godot-haskell-classgen -- ../godot_headers/api.json ../godot_doc_classes.json ../" 2>&1 | tee ../errorLog
+  cd -
+}
