59ee3ab56ec244a1a56f4a72414fe675c0e2d5d6
Jonny Ramsden <JonnyRa@users.noreply.github.com>
2026-08-08 00:30:32 +0100

    add an equivalent to the make file build to the top level.  This now works but there is a problem running, something to do with the signals

--- godot.bash
+++ godot.bash
@@ -17,3 +17,9 @@ rebuildGodotTypes () {
   stack build --colour always --file-watch --watch-all --exec  "stack exec godot-haskell-classgen -- ../godot_headers/api.json ../godot_doc_classes.json ../" 2>&1 | tee ../errorLog
   cd -
 }
+
+buildDodgeTheCreeps(){
+  STACKLIBFILE=$(stack path --local-install-root)/lib/libmyproject.so
+  GODOTPROJECT=$(stack path --project-root)/examples/dodge-the-creeps/game
+  stackBuildWatch myproject --exec "cp $STACKLIBFILE $GODOTPROJECT/lib"
+}
