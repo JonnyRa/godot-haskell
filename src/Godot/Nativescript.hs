80eba43bad9ad417b9705526f4ddfa5ec256c4bb
Jonny Ramsden <JonnyRa@users.noreply.github.com>
2026-08-07 22:42:21 +0100

    FIXES BUILD split types out to their own file, this is so that the deriveBase splice can be in the right place

--- src/Godot/Nativescript.hs
+++ src/Godot/Nativescript.hs
@@ -98,58 +98,7 @@ import           Foreign.StablePtr
 import           Control.Monad.Extra
 import           Control.Exception
 import           Data.Maybe
-
-type GdnativeHandle = Ptr ()
-
-data GodotError = GFailed -- ^ Generic error.
-                | GUnavailable -- ^ Unavailable error.
-                | GUnconfigured -- ^ Unconfigured error.
-                | GUnauthorized -- ^ Unauthorized error.
-                | GParameterRangeError -- ^ Parameter range error.
-                | GOutOfMemory -- ^ Out of memory (OOM) error.
-                | GFileNotFound -- ^ File: Not found error.
-                | GFileBadDrive -- ^ File: Bad drive error.
-                | GFileBadPath -- ^ File: Bad path error.
-                | GFileNoPermission -- ^ File: No permission error.
-                | GFileAlreadyInUse -- ^ File: Already in use error.
-                | GFileCantOpen -- ^ File: Can't open error.
-                | GFileCantWrite -- ^ File: Can't write error.
-                | GFileCantRead -- ^ File: Can't read error.
-                | GFileUnrecognized -- ^ File: Unrecognized error.
-                | GFileCorrupt -- ^ File: Corrupt error.
-                | GFileMissingDependencies -- ^ File: Missing dependencies error.
-                | GFileEof -- ^ File: End of file (EOF) error.
-                | GCantOpen -- ^ Can't open error.
-                | GCantCreate -- ^ Can't create error.
-                | GQueryFailed -- ^ Query failed error.
-                | GAlreadyInUse -- ^ Already in use error.
-                | GLocked -- ^ Locked error.
-                | GTimeout -- ^ Timeout error.
-                | GCantConnect -- ^ Can't connect error.
-                | GCantResolve -- ^ Can't resolve error.
-                | GConnectionError -- ^ Connection error.
-                | GCantAcquireResource -- ^ Can't acquire resource error.
-                | GCantFork -- ^ Can't fork process error.
-                | GInvalidData -- ^ Invalid data error.
-                | GInvalidParameter -- ^ Invalid parameter error.
-                | GAlreadyExists -- ^ Already exists error.
-                | GDoesNotExist -- ^ Does not exist error.
-                | GDatabaseCantRead -- ^ Database: Read error.
-                | GDatabaseCantWrite -- ^ Database: Write error.
-                | GCompilationFailed -- ^ Compilation failed error.
-                | GMethodNotFound -- ^ Method not found error.
-                | GLinkFailed -- ^ Linking failed error.
-                | GScriptFailed -- ^ Script failed error.
-                | GCyclicLink -- ^ Cycling link (import cycle) error.
-                | GInvalidDeclaration -- ^ Invalid declaration error.
-                | GDuplicateSymbol -- ^ Duplicate symbol error.
-                | GParseError -- ^ Parse error.
-                | GBusy -- ^ Busy error.
-                | GSkip -- ^ Skip error.
-                | GHelp -- ^ Help error.
-                | GBug -- ^ Bug error.
-                | GPrinterOnFire -- ^ Printer on fire error. (This is an easter egg, no engine methods return this error code.)
-                deriving (Show, Eq, Typeable)
+import Godot.Nativescript.Types
 
 instance Exception GodotError
 
@@ -246,77 +195,6 @@ instance NativeScript MyClass1 where
 deriveBase ''Myclass1
 @
 -}
-class (HasBaseClass cls, Typeable cls, Typeable (BaseClass cls), Object :< cls)
-  => NativeScript cls where
-  classInit :: BaseClass cls -> IO cls
-  className :: ClassName cls
-  className = nameOf @cls
-  classMethods :: [ClassMethod cls]
-  classSignals :: [ClassSignal]
-  classSignals = []
-  classProperties :: [ClassProperty cls]
-  classProperties = []
-  asObj :: cls -> Object
-  asObj = upcast
-
-type ClassSignal = (Text, [SignalArgument])
-
-type ClassName a = Text
-nameOf :: forall a . Typeable a => ClassName a
-nameOf = T.pack
-    $ convertClassName
-    $ show
-    $ typeRep
-    $ Proxy @a
-
-
-type GFunc cls = cls -> Vector GodotVariant -> IO GodotVariant
-
-data ClassMethod cls where
-  ClassMethod
-    :: { methodRPCMode :: RPC
-       , methodName :: Text
-       , methodFunc :: GFunc cls
-       }
-    -> ClassMethod cls
-
-
-data RPC
-  = NoRPC
-  | Remote
-  | Sync
-  | Master
-  | Slave
-
-
-data RegTy
-  = GClass
-  | GMethod
-  | GSignal
-  | GProperty
-
-data family Registerer (x :: RegTy) cls
-
-data instance Registerer 'GClass cls = NativeScript cls =>
-  RegClass
-    GdnativeHandle
-    (BaseClass cls -> IO cls)
-
-data instance Registerer 'GMethod cls = NativeScript cls =>
-  RegMethod
-    GdnativeHandle
-    (ClassMethod cls)
-
-data instance Registerer 'GSignal cls = NativeScript cls =>
-  RegSignal
-    GdnativeHandle
-    (Text, [SignalArgument])
-
-data instance Registerer 'GProperty cls = NativeScript cls =>
-  RegProperty
-    GdnativeHandle
-    (ClassProperty cls)
-
 
 -- | Convenient way of registering a class with all its methods.
 -- Used like: @registerClass $ RegClass desc $ classInit \\@MyClass@
@@ -392,37 +270,6 @@ tryCast = tryObjectCast . upcast
 
 tryCast' :: forall out x. (Typeable out, AsVariant out, Object :< x, x :< out) => x -> IO out
 tryCast' o = fromJust <$> tryCast o
-
-convertClassName name =
-  case name of
-    -- TODO Derive these automatically. Don't think we store if a
-    -- class is a singleton anywhere.
-    "ClassDB" -> "_ClassDB"
-    "Marshalls" -> "_Marshalls"
-    "VisualScriptEditor" -> "_VisualScriptEditor"
-    "OS" -> "_OS"
-    "Engine" -> "_Engine"
-    "Geometry" -> "_Geometry"
-    "JSON" -> "_JSON"
-    "ResourceLoader" -> "_ResourceLoader"
-    "ResourceSaver" -> "_ResourceSaver"
-    x -> x
-
-unConvertClassName name =
-  case name of
-    -- TODO Derive these automatically. Don't think we store if a
-    -- class is a singleton anywhere.
-    "_ClassDB" -> "ClassDB"
-    "_Marshalls" -> "Marshalls"
-    "_VisualScriptEditor" -> "VisualScriptEditor"
-    "_OS" -> "OS"
-    "_Engine" -> "Engine"
-    "_Geometry" -> "Geometry"
-    "_JSON" -> "JSON"
-    "_ResourceLoader" -> "ResourceLoader"
-    "_ResourceSaver" -> "ResourceSaver"
-    x -> x
-
 singletonTable :: MVar (M.Map Text Object)
 singletonTable = unsafePerformIO $ newMVar M.empty
 
@@ -691,15 +538,6 @@ createMVarProperty name fieldName tyOrVal = ClassProperty
      Left vTy -> (vTy, VariantNil)
      Right val -> (variantType val, toVariant val)
 
-data SignalArgument = SignalArgument
-  { signalArgumentName :: !Text
-  , signalArgumentType :: !VariantType
-  , signalArgumentHint :: !PropertyHint
-  , signalArgumentHintString :: !Text
-  , signalArgumentUsage :: !PropertyUsageFlags
-  , signalArgumentDefaultValue :: !(Variant 'GodotTy)
-  }
-
 asSignalArgument :: SignalArgument -> IO GNI.SignalArgument
 asSignalArgument SignalArgument {..} = do
   name <- toLowLevel signalArgumentName
@@ -846,14 +684,3 @@ await self target signal fn = do
     <*> (Just <$> toLowLevel (V.singleton fnBind))
     <*> pure (Just _CONNECT_ONESHOT))
 
--- | Sometimes we really have to pass in a Haskell pointer to Godot,
--- particularly for callbacks.
-data WrapperStablePtr = WrapperStablePtr { _wrapperStablePtrBase :: Object
-                                , _wrapperStablePtr :: MVar (StablePtr ()) }
-instance HasBaseClass WrapperStablePtr where
-  type BaseClass WrapperStablePtr = Object
-  super = _wrapperStablePtrBase
-instance NativeScript WrapperStablePtr where
-  classInit base = WrapperStablePtr base <$> newEmptyMVar
-  classMethods = []
-deriveBase ''WrapperStablePtr
